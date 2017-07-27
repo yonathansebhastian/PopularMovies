b6f47a4e7b132a8ef556218dc159ac3f8d92255b
yonathansebhastian <yonathansebhastian@gmail.com>
2017-07-27 17:58:31 +0700

    optimize import

--- app/src/main/java/com/sebhastian/popularmoviesdatabase/MainActivityFragment.java
+++ app/src/main/java/com/sebhastian/popularmoviesdatabase/MainActivityFragment.java
@@ -13,7 +13,6 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
