13e28e83ccbab7b11ba03577abdb53899eac4f27
yonathansebhastian <yonathansebhastian@gmail.com>
2017-07-27 16:16:37 +0700

    Project 2 OK

---
+++ app/src/main/java/com/sebhastian/popularmoviesdatabase/Constants.java
@@ -0,0 +1,19 @@
+package com.sebhastian.popularmoviesdatabase;
+
+/**
+ * Created by Yonathan Sebhastian on 7/22/2017.
+ */
+
+public final class Constants {
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    public static final String APP_KEY_QUERY_PARAM = "api_key";
+    public static final String API_KEY = "6ebb54f8e2f0dc947db5f8755fe8b924";
+    public static final String SORT_POPULARITY = "popular";
+    public static final String SORT_RATING = "top_rated";
+    public static final String SORT_FAVES = "favourites";
+    public final static String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+    public final static String IMAGE_SMALL_SIZE = "w185";
+    public final static String RATING_MAX = "10";
+    public final static String YOUTUBE_PREFIX = "http://www.youtube.com/watch?v=";
+
+}
--- app/src/main/java/com/sebhastian/popularmoviesdatabase/db/MovieContract.java
+++ app/src/main/java/com/sebhastian/popularmoviesdatabase/db/MovieContract.java
@@ -1,8 +1,52 @@
 package com.sebhastian.popularmoviesdatabase.db;
 
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 /**
  * Created by Yonathan Sebhastian on 7/22/2017.
  */
 
 public class MovieContract {
+    public static final String CONTENT_AUTHORITY = "com.sebhastian.popularmoviesdatabase";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_MOVIE = "movies";
+
+    private MovieContract() {
+    }
+
+    public static final class MovieEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static final String TABLE_NAME = "movies";
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_MOVIE_TITLE = "original_title";
+        public static final String COLUMN_MOVIE_POSTER_PATH = "poster_path";
+        public static final String COLUMN_MOVIE_OVERVIEW = "overview";
+        public static final String COLUMN_MOVIE_VOTE_AVERAGE = "vote_average";
+        public static final String COLUMN_MOVIE_RELEASE_DATE = "release_date";
+
+        public static final int COLUMN_MOVIE_ID_INDEX = 0;
+        public static final int COLUMN_MOVIE_TITLE_INDEX = 1;
+        public static final int COLUMN_MOVIE_POSTER_PATH_INDEX = 2;
+        public static final int COLUMN_MOVIE_OVERVIEW_INDEX = 3;
+        public static final int COLUMN_MOVIE_VOTE_AVERAGE_INDEX = 4;
+        public static final int COLUMN_MOVIE_RELEASE_DATE_INDEX = 5;
+
+        public static final String[] MOVIE_COLUMNS = {
+                COLUMN_MOVIE_ID,
+                COLUMN_MOVIE_TITLE,
+                COLUMN_MOVIE_POSTER_PATH,
+                COLUMN_MOVIE_OVERVIEW,
+                COLUMN_MOVIE_VOTE_AVERAGE,
+                COLUMN_MOVIE_RELEASE_DATE
+        };
+
+    }
 }
